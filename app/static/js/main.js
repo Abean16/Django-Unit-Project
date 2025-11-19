4f53446064d3f2ee8c056438a56d14b433bece42
Aleah <abean26@basecampcodingacademy.org>
2025-11-19 10:09:30 -0600

    javascript & html

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,16 +1,39 @@
 const boxes = document.querySelectorAll('.item-box');
-const desc = document.querySelectorAll('item-desc')
+const descs = document.getElementsByClassName('.item-desc');
+const button = document.getElementById('button')
+
 for (let i = 0; i < boxes.length; i++) {
     const currentBox = boxes[i];
         currentBox.addEventListener("mouseenter", function(){
             currentBox.style.paddingBottom = "20%";
             currentBox.style.transform = "scaleY(1.0)";
             currentBox.style.transition = "1s";
-            currentDesc.style.display == "block";
         })
         currentBox.addEventListener("mouseleave", function(){
             setTimeout(() => {
                 currentBox.style.paddingBottom = "1%";
+                currentBox.style.display == "none";
+            }, 500);
+        })
+}
+
+
+button.addEventListener("click", function(){
+    for (let i = 0; i < boxes.length; i++) {
+    const currentBox = boxes[i];
+        descs.style.display = "block"
+})
+
+
+for (let i = 0; i < descs.length; i++) {
+    const currentDesc = descs[i];
+        currentDesc.addEventListener("mouseenter", function(){
+            currentDesc.style.display == "block";
+            currentDesc.style.color = "aqua"
+            currentDesc.style.backgroundColor = "aqua"
+        })
+        currentDesc.addEventListener("mouseleave", function(){
+            setTimeout(() => {
                 currentDesc.style.display == "none";
             }, 500);
         })

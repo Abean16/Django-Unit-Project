fa136e5e50919b8a521fb33487e963711fcad80a
Aleah <abean26@basecampcodingacademy.org>
2025-11-28 21:57:13 -0600

    glitch fix, nav fix

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,28 +1,29 @@
 const boxes = document.querySelectorAll('.item-box');
 const descs = document.querySelectorAll('.item-desc');
-const innerBoxes = document.querySelectorAll('.item-box-inner')
 
 for (let i = 0; i < boxes.length; i++){
     const currentBox = boxes[i];
     const currentDesc = descs[i];
-    const currentInnerBox = innerBoxes[i]
+    let timeout;
 
     currentBox.addEventListener("mouseenter", function(){
-        currentBox.style.paddingBottom = "25%";
-        currentBox.style.transform = "scaleY(1.0)";
-        currentBox.style.transition = "1s";
-        setTimeout(() => {
+        clearTimeout(timeout);
+
+        currentBox.style.transition = "padding-bottom 1s";
+        currentBox.style.paddingBottom = "25%"; 
+
+        timeout = setTimeout(() => {
             currentDesc.style.display = "flex";  
-            currentDesc.style.animation = "fadeIn 1s"
+            currentDesc.style.animation = "fadeIn 1s";
         }, 450);
     });
 
     currentBox.addEventListener("mouseleave", function(){
-        setTimeout(() => {
+        clearTimeout(timeout); 
+
+        timeout = setTimeout(() => {
             currentBox.style.paddingBottom = "1%";
             currentDesc.style.display = "none"; 
         }, 100);
     });
-    
 }
-
